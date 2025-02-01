31b05644dbeee90f3b94cf44fd7dd34f6167caac
KunBuFenZi <1462632457@qq.com>
2025-02-02 00:39:59 +0800

    Update Footer.tsx

--- apps/remix/app/components/Footer.tsx
+++ apps/remix/app/components/Footer.tsx
@@ -9,13 +9,13 @@ export default function Footer() {
   return (
     <div className="text-white w-full mt-auto flex flex-col items-center justify-between px-5 pt-16 mb-10 md:px-10 mx-auto sm:flex-row">
       <Link to="/" className="text-xl font-black leading-none select-none logo">
-        VMAIL.DEV
+        MAIL.CPU.RED
       </Link>{" "}
       <p className="mt-4 text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">
         © 2024 Products of{" "}
         <Link
           className="font-semibold underline hover:text-gray-600"
-          to="https://www.oiov.dev"
+          to="https://mail.cpu.red"
           target="_blank">
           oiov
         </Link>
