1d6d1f8c9add5dc06fecd0a513df94ba86595678
KunBuFenZi <1462632457@qq.com>
2025-02-02 00:38:00 +0800

    Update Footer.tsx

--- apps/remix/app/components/Footer.tsx
+++ apps/remix/app/components/Footer.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
         © 2024 Products of{" "}
         <Link
           className="font-semibold underline hover:text-gray-600"
-          to="https://www.cpu.red"
+          to="https://www.oiov.dev"
           target="_blank">
           oiov
         </Link>
@@ -23,38 +23,12 @@ export default function Footer() {
       </p>
       <div className="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start">
         <Link
-          to="https://wr.do"
-          target="_blank"
-          title="WR.DO"
-          className="text-gray-400 hover:text-gray-500  scale-[1.2]">
-          <WrdoLogo className="w-6 h-6" />
-        </Link>
-        <Link
-          to="mailto:hi@oiov.dev"
-          title="Email"
-          className="text-gray-400 hover:text-gray-500">
-          <MailIcon className="w-6 h-6" />
-        </Link>
-        <Link
-          to="https://twitter.com/yesmoree"
-          target="_blank"
-          title="Twitter"
-          className="text-gray-400 hover:text-gray-500">
-          <Twitter />
-        </Link>
-        <Link
           to="https://github.com/oiov/vmail"
           target="_blank"
           title="Github"
           className="text-gray-400 hover:text-gray-500">
           <Github />
         </Link>
-        {/* <Link
-          to="https://www.buymeacoffee.com/yesmore"
-          target="_blank"
-          title="Buy me a coffee">
-          <Coffee className="w-6 h-6" />
-        </Link> */}
       </div>
     </div>
   );
